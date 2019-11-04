5d7adce133e666e57c83d206e7a268f1896d6e22
Mark Tellez <mark.tellez@icloud.com>
2019-11-04 09:09:52 -0600

    done

--- src/components/cart/context.js
+++ src/components/cart/context.js
@@ -9,10 +9,26 @@ export default function CartProvider({ children }) {
     setItems(prevState => [...prevState, item])
   }
 
+  function itemsWithQuantities(items) {
+    return items.reduce((acc, item) => {
+      const found = acc.find(_item => _item.sku === item.sku)
+      if (found) {
+        found.quantity = found.quantity + 1
+      } else {
+        acc.push({
+          sku: item.sku,
+          quantity: 1,
+          price: item.price
+        })
+      }
+      return acc
+    }, [])
+  }
+
   return (
     <CartContext.Provider
       value={{
-        items,
+        items: itemsWithQuantities(items),
         addToCart
       }}
     >
